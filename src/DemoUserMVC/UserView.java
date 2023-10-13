78f10c7fd51b109ba6f33d70cf064939b4f384de
Mrjaider <jaideraldana@hotmail.com>
2023-10-13 09:12:09 -0500

    New user interface

--- src/DemoUserMVC/UserView.java
+++ src/DemoUserMVC/UserView.java
@@ -1,21 +1,25 @@
 package DemoUserMVC;
 
-import java.util.Scanner;
+import java.util.*;
+
+import javax.swing.JOptionPane;
 
 public class UserView {
 
     public void printUserDetails(User user) {
-        System.out.println("\nUser details: ");
-        System.out.println("Username: " + user.getUsername());
-        System.out.println("Password: " + user.getPassword());
-        System.out.println("Email: " + user.getEmail());
+
+        String message = "\nUser details: \n \n";
+        message += "Username: " + user.getUsername() + "\n";
+        message += "Password: " + user.getPassword() + "\n";
+        message += "Email: " + user.getEmail() + "\n";
+
+        JOptionPane.showMessageDialog(null, message);
 
     }
 
     public String getInput(String message) {
-        System.out.println(message);
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+
+        return JOptionPane.showInputDialog(null, message);
     }
 
     public void showMessages(String message) {
@@ -25,7 +29,7 @@ public class UserView {
     public void displayMenu() {
         System.out.println("\nMenu:");
         System.out.println("1. Create an user");
-        System.out.println("2. consult an user");
+        System.out.println("2. Consult an user");
         System.out.println("3. Modify an user");
         System.out.println("4. Delete an user");
         System.out.println("5. Exit");
